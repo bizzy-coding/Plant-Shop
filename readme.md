fefc457d5d11363e1a71f25d419e7d78be60a3f1
Bizzy-coding <bizzycoding@gmail.com>
2023-05-23 21:55:55 +0100

    updating readme file

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Plant Shop
 
-Vanilla javascript project, utilising objects to creature product listings. 
+Vanilla javascript project, utilising objects and sorting methhods to create a plant shop. 
 
 Featuring:
 - object methods
@@ -9,18 +9,46 @@ Featuring:
 
 👀 [LIVE PREVIEW](https://bizzy-coding.github.io/) coming soon....
 
-A new space to showcase my best work
-
-
-
 ## Next steps
 
-[] Create product object
-[] Basic Styling
-[] Display products 
+[x] Create product object
+[x] Basic Styling
+[x] Display products 
+[x] Sorting functionality
+
+## Functionality utilised
+
+1. To sort the plants array alphabetically (A-Z) based on their names:
+   - Use the `sort()` method with a comparison function that compares the `name` property of each plant using `localeCompare()`.
+
+2. To implement a button for sorting the plants:
+   - Create a separate array (`originalPlants`) to preserve the original order.
+   - Use the original array to create and append the product cards initially.
+   - Add a button element to trigger the sort functionality.
+
+3. When the sort button is clicked to sort the plants alphabetically:
+   - Sort the `originalPlants` array using `sort()` with a comparison function that compares the `name` property.
+   - Clear the existing product cards and regenerate them in the sorted order.
+
+4. To sort the plants based on price (high to low):
+   - Add a button element to trigger the sort functionality.
+   - Sort the `originalPlants` array using `sort()` with a comparison function that subtracts `b.price` from `a.price`.
+   - Regenerate the product cards in the sorted order.
+
+5. To sort the plants based on price (low to high):
+   - Add a button element to trigger the sort functionality.
+   - Sort the `originalPlants` array using `sort()` with a comparison function that subtracts `a.price` from `b.price`.
+   - Regenerate the product cards in the sorted order.
+
+   ## Acknowledgments
+
+Sources used
+* [MDN WEB DOCS - String.prototype.localeCompare()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare)
+* [MDN WEB DOCS - Sort()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
 
 
-## 🚀 About Me
+
+# 🚀 About Me
 
 Hi. I'm Laura Bizzle, but you can call me Bizz and I am always [Bizzy Coding](https://www.instagram.com/bizzy_coding/) 
 
@@ -35,8 +63,3 @@ Coding is life
 [![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/laura-bizzle/)
 [![instagram](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/bizzy_coding/)
 
-## Acknowledgments
-
-Inspiration, code snippets, etc.
-* [coming soon](https://www.youtube.com/watch?v=Ttf3CEsEwMQ)
-* [coming soon](https://gist.github.com/DomPizzie/7a5ff55ffa9081f2de27c315f5018afc#file-readme-template-md)
--- scripts.js
+++ scripts.js
@@ -1,109 +1,96 @@
+//what am I trying to acheive here?
+//writing this down helps to plan the project
+//loop through the object and create a new products
+//each product should be a product card within the prod container
+
 let plants = [
-    {
-      name: "Outdoor Palm",
-      description:
-        "Add a tropical touch to your home with this artificial palm plant. With its lush green leaves and realistic texture, it's the perfect way to bring the outdoors inside.",
-      price: 29.99,
-      imageURL:
-        "https://www.ikea.com/gb/en/images/products/fejka-artificial-potted-plant-in-outdoor-palm__0902075_pe689305_s5.jpg?f=xxxs",
-      inStock: false
-    },
-    {
-      name: "Epipremnum Golden Pothos",
-      description:
-        "Brighten up your space with this stunning golden pothos plant. Its cascading leaves will add a pop of green to any room, and its low maintenance nature makes it the perfect choice for busy households.",
-      price: 14.99,
-      imageURL:
-        "https://www.ikea.com/gb/en/images/products/epipremnum-potted-plant-golden-pothos__0945915_pe797829_s5.jpg?f=xs",
-      inStock: true
-    },
-    {
-      name: "String of Beads",
-      description:
-        "Get the look of real hanging plants without the hassle with this artificial string of beads plant. With its delicate beads cascading down, it's sure to add a touch of charm to any room.",
-      price: 21.99,
-      imageURL:
-        "https://www.ikea.com/gb/en/images/products/fejka-artificial-potted-plant-in-outdoor-string-of-beads__0901067_pe687832_s5.jpg?f=xxxs",
-      inStock: true
-    },
-    {
-      name: "Hanging String of Hearts",
-      description:
-        "Bring the romance of the outdoors inside with this artificial string of hearts plant. Its delicate heart-shaped leaves are sure to add a touch of charm to any space.",
-      price: 19.99,
-      imageURL:
-        "https://www.ikea.com/gb/en/images/products/fejka-artificial-potted-plant-in-outdoor-hanging-string-of-hearts__0817873_pe774217_s5.jpg?f=xxxs",
-      inStock: true
-    },
-    {
-      name: "Ivy Potted Plant",
-      description:
-        "Add a touch of greenery to your home without the maintenance with this artificial ivy plant. With its realistic texture and lush green leaves, it's the perfect way to bring the outdoors inside.",
-      price: 12.99,
-      imageURL:
-        "https://www.ikea.com/gb/en/images/products/fejka-artificial-potted-plant-ivy__0901324_pe668406_s5.jpg?f=xxxs",
-      inStock: true
-    },
-    {
-      name: "Island Date Palm",
-      description:
-        "Bring a touch of the exotic to your home with this stunning Canary Island date palm. With its dramatic fronds and impressive height, it's sure to make a statement in any room.",
-      price: 35.0,
-      imageURL:
-        "https://www.ikea.com/gb/en/images/products/phoenix-canariensis-potted-plant-canary-island-date-palm__0958848_pe805499_s5.jpg?f=xxxs",
-      inStock: true
-    },
-    
-    {
+  {
+    name: "Outdoor Palm",
+    description: "Add a tropical touch to your home with this artificial palm plant. With its lush green leaves and realistic texture, it's the perfect way to bring the outdoors inside.",
+    price: 29.99,
+    imageURL: "https://www.ikea.com/gb/en/images/products/fejka-artificial-potted-plant-in-outdoor-palm__0902075_pe689305_s5.jpg?f=xxxs",
+    inStock: false,
+  },
+  {
+    name: "Epipremnum Golden Pothos",
+    description: "Brighten up your space with this stunning golden pothos plant. Its cascading leaves will add a pop of green to any room, and its low maintenance nature makes it the perfect choice for busy households.",
+    price: 14.99,
+    imageURL: "https://www.ikea.com/gb/en/images/products/epipremnum-potted-plant-golden-pothos__0945915_pe797829_s5.jpg?f=xs",
+    inStock: true,
+  },
+  {
+    name: "String of Beads",
+    description: "Get the look of real hanging plants without the hassle with this artificial string of beads plant. With its delicate beads cascading down, it's sure to add a touch of charm to any room.",
+    price: 21.99,
+    imageURL: "https://www.ikea.com/gb/en/images/products/fejka-artificial-potted-plant-in-outdoor-string-of-beads__0901067_pe687832_s5.jpg?f=xxxs",
+    inStock: true,
+  },
+  {
+    name: "Hanging String of Hearts",
+    description: "Bring the romance of the outdoors inside with this artificial string of hearts plant. Its delicate heart-shaped leaves are sure to add a touch of charm to any space.",
+    price: 19.99,
+    imageURL: "https://www.ikea.com/gb/en/images/products/fejka-artificial-potted-plant-in-outdoor-hanging-string-of-hearts__0817873_pe774217_s5.jpg?f=xxxs",
+    inStock: true,
+  },
+  {
+    name: "Ivy Potted Plant",
+    description: "Add a touch of greenery to your home without the maintenance with this artificial ivy plant. With its realistic texture and lush green leaves, it's the perfect way to bring the outdoors inside.",
+    price: 12.99,
+    imageURL: "https://www.ikea.com/gb/en/images/products/fejka-artificial-potted-plant-ivy__0901324_pe668406_s5.jpg?f=xxxs",
+    inStock: true,
+  },
+  {
+    name: "Island Date Palm",
+    description: "Bring a touch of the exotic to your home with this stunning Canary Island date palm. With its dramatic fronds and impressive height, it's sure to make a statement in any room.",
+    price: 35.0,
+    imageURL: "https://www.ikea.com/gb/en/images/products/phoenix-canariensis-potted-plant-canary-island-date-palm__0958848_pe805499_s5.jpg?f=xxxs",
+    inStock: true,
+  },
+
+  {
     name: "Epipremnum Potted Plant",
     description: "This versatile houseplant is perfect for beginners and adds a pop of green to any room. With its trailing vines and heart-shaped leaves, it's easy to care for and thrives in a variety of lighting conditions.",
     price: 19.99,
     imageURL: "https://www.ikea.com/gb/en/images/products/epipremnum-potted-plant-golden-pothos__0900460_pe668029_s5.jpg?f=xxxs",
-    inStock: true
+    inStock: true,
   },
   {
     name: "Phoenix Canariensis",
     description: "Bring a tropical touch to your space with this striking Canary Island Date Palm. Its large, feathery fronds make it a statement piece in any room or outdoor area. Best of all, it requires little maintenance!",
     price: 29.99,
     imageURL: "https://www.ikea.com/gb/en/images/products/phoenix-canariensis-potted-plant-canary-island-date-palm__0958848_pe805499_s5.jpg?f=xxxs",
-    inStock: true
+    inStock: true,
   },
   {
     name: "Dracaena Marginata Plant",
     description: "With its tall, slender stems and spiky leaves, the Dragon Tree adds a touch of drama to any space. It's a slow grower, so it's perfect for those who prefer low-maintenance plants.",
     price: 24.99,
     imageURL: "https://www.ikea.com/gb/en/images/products/dracaena-marginata-potted-plant-dragon-tree-2-stem__0902168_pe654240_s5.jpg?f=xxxs",
-    inStock: true
+    inStock: true,
   },
   {
     name: "Whitley Giant Potted Plant",
     description: "Make a statement with this bold and beautiful Whitley Giant. With its large, glossy leaves and vibrant green color, it's sure to be a conversation starter. Best of all, it's easy to care for!",
     price: 34.99,
     imageURL: "https://www.ikea.com/gb/en/images/products/fejka-artificial-potted-plant-in-outdoor-whitley-giant__0959239_pe809450_s5.jpg?f=xxxs",
-    inStock: true
-  }
-  
-  ];
+    inStock: true,
+  },
+];
 
-  const originalPlants = [...plants];
+//product object must be reassigned for the sorting functionality
+const originalPlants = [...plants];
 
-  const prodCont = document.querySelector('.product--container');
+const prodCont = document.querySelector(".product--container");
 
-  //what am I trying to acheive here?
-
-  //loop through the object and create a new products
-  //each product should be a product card within the prod container 
-
-  function createProdCards() {
-
-    originalPlants.forEach((plant) => {
-        console.log(plant.price)
+function createProdCards() {
+  originalPlants.forEach((plant) => {
+    console.log(plant.price);
     //now you can access each plant and each of it's properties
-        var prodCard = document.createElement('div');
-        prodCard.classList.add('product--card');
-        prodCont.appendChild(prodCard);
+    var prodCard = document.createElement("div");
+    prodCard.classList.add("product--card");
+    prodCont.appendChild(prodCard);
     //for each of the products, a card has been created, but this is just empty until you fill it
-        prodCard.innerHTML = `
+    prodCard.innerHTML = `
         <h3>${plant.name}</h3>
         <div class="product--image">
         <img src=${plant.imageURL}/>
@@ -111,50 +98,42 @@ let plants = [
         </div>
         <p>${plant.description}</p>
         <p>${plant.price}</p>
-        `
-      });
+        `;
+  });
+}
 
-  }
+createProdCards();
 
-  createProdCards();
+//A-Z SORTING FUNCTIONALITY
 
+const sortButtonAZ = document.querySelector(".sort--button--AZ");
+sortButtonAZ.addEventListener("click", sortPlantsAZ);
 
-  //A-Z SORTING FUNCTIONALITY
-
-  const sortButtonAZ = document.querySelector('.sort--button--AZ')
-  sortButtonAZ.addEventListener('click' , sortPlantsAZ);
-
-  function sortPlantsAZ() {
-      // Sort the originalPlants array alphabetically
+function sortPlantsAZ() {
+  // Sort the originalPlants array alphabetically
   originalPlants.sort((a, b) => a.name.localeCompare(b.name));
 
   // Clear the existing product cards
-  prodCont.innerHTML = '';
-  createProdCards();
-  }
-
-  //PRICE SORTING FUNCTIONALITY 
-
-  const sortButtonHighLow = document.querySelector('.sort--button--high--low');
-  sortButtonHighLow.addEventListener('click' , sortPlantsHighLow);
-
-  function sortPlantsHighLow() {
-    originalPlants.sort((a,b) => b.price - a.price);
-    prodCont.innerHTML = '';
-    createProdCards();
-  }
-
-  const sortButtonLowHigh = document.querySelector('.sort--button--low--high');
-sortButtonLowHigh.addEventListener('click', sortPlantsLowHigh);
-
-function sortPlantsLowHigh() {
-  originalPlants.sort((a, b) => a.price - b.price);
-  prodCont.innerHTML = '';
+  prodCont.innerHTML = "";
   createProdCards();
 }
 
+//PRICE SORTING FUNCTIONALITY
 
+const sortButtonHighLow = document.querySelector(".sort--button--high--low");
+sortButtonHighLow.addEventListener("click", sortPlantsHighLow);
 
+function sortPlantsHighLow() {
+  originalPlants.sort((a, b) => b.price - a.price);
+  prodCont.innerHTML = "";
+  createProdCards();
+}
 
+const sortButtonLowHigh = document.querySelector(".sort--button--low--high");
+sortButtonLowHigh.addEventListener("click", sortPlantsLowHigh);
 
-  
+function sortPlantsLowHigh() {
+  originalPlants.sort((a, b) => a.price - b.price);
+  prodCont.innerHTML = "";
+  createProdCards();
+}
